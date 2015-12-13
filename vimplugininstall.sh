123271399f9cbb491c3e9303079dfcaba3887ef5
Adam Bengtsson <adampkb@gmail.com>
2015-12-13 07:50:53 -0800

    removed faulty line

--- vimplugininstall.sh
+++ vimplugininstall.sh
@@ -7,7 +7,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
  
 
-git clone git://github.com/tpope/vim-fireplace.git
+git clone https://github.com/tpope/vim-fireplace.git
 git clone https://github.com/guns/vim-clojure-static.git
 git clone https://github.com/guns/vim-clojure-highlight.git
 git clone https://github.com/vim-scripts/paredit.vim 
